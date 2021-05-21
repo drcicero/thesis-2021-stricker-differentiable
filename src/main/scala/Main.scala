12f0d086c1bfe38ef8ac53331b00cf382054c0fa
AraneusRota <araneusrota.github@paluk.org>
2021-05-21 02:37:12 +0200

    #1: Initial try to differentiate polynomials at compile time

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,40 +1,11 @@
 import scala.quoted._
-import Poly._
-import Macros.checkSimplePolynomial
+import polynomial.Polyinomial._
+import polynomial._
 
-enum Poly:
-  def +(r: Poly) = Plus(this, r)
-  def *(r: Poly) = Mul(this, r)
-  def ^(n: Int) = Pow(this, n)
+case class &[L, R](l: L, r: R)
 
-  case V(v: Double)
-  case Plus(l: Poly, r: Poly)
-  case Mul(l: Poly, r: Poly)
-  case Pow(x: Poly, n: Int)
-  case X
-
-@main def main: Unit =
-  checkSimplePolynomial(
-    V(5) + V(1)*X + (X^5) + V(-6.5)*(X^(5)) + (X^2) + X
-  )
-
-  checkSimplePolynomial(
-    X^0
-  )
-  checkSimplePolynomial(
-    X^(-1)
-  )
-  checkSimplePolynomial(
-    (X^5) * V(1)
-  )
-  val notKnownAtCompileTime = 5
-  checkSimplePolynomial(
-    X^(notKnownAtCompileTime)
-  )
-  checkSimplePolynomial(
-    X * X
-  )
-  checkSimplePolynomial(
-    V(5)^5
-  )
+@main def main: String =
+  val x: V[2.2] * V[5.0] = initPolynomial
+  val s: V[5.5] * V[6] * (X^5) = d[V[5.5] * (X^6)]
+  ""
   
