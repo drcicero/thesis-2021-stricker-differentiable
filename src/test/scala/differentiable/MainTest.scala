3e83fa63b12d51e655cc3b11e345d554428d03d6
AraneusRota <araneusrota.github@paluk.org>
2021-05-28 23:29:08 +0200

    #1 test: Differentiate polynomials at type level (at compile time) with match types

---
+++ src/test/scala/differentiable/MainTest.scala
@@ -0,0 +1,42 @@
+package differentiable
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalactic.TolerantNumerics.tolerantDoubleEquality
+import org.scalactic.Equality
+import differentiable._
+
+class MainSuite extends AnyFunSuite {
+  implicit val doubleEq: Equality[Double] = tolerantDoubleEquality(0.0001)
+
+  test("polynomial differentiation") {
+    val cases = Seq(
+      d[X * X] -> initPolynomial[
+        V[2] * X
+      ],
+      d[V[5.5] * (X^3)] -> initPolynomial[
+        V[16.5] * (X^2)
+      ],
+      d[(X^3) * (X^2) * (X^1)] -> initPolynomial[
+        V[6] * (X ^ 5)
+      ],
+      d[(X^3) * (X ^ -2) * (X^1)] -> initPolynomial[
+        V[2] * (X^1)
+      ],
+      d[V[0] + V[0] * V[-0.1] + (X ^ -5) * V[0]] -> initPolynomial[
+        V[0]
+      ],
+      d[X * V[5]] -> initPolynomial[
+        V[5]
+      ],
+      // compile time is much longer with 4 multiplications
+      d[V[0] + V[0] * V[-0.1] + (X ^ 10) * (X ^ 0) * (X ^ 2) + (X^3) * X * (X^2)] -> initPolynomial[
+        V[12] * (X^11) + V[6] * (X^5)
+      ]
+    )
+    cases.foreach { (actual, expected) =>
+      (-5 to 5).foreach { x =>
+        assert(actual(x) === expected(x), s"\nx = $x\nActual: $actual\nExpected: $expected")
+      }
+    }
+  }
+}
