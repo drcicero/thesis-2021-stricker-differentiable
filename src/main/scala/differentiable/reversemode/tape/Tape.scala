cbce892aa43cdef67e4ca72e7e5536d3b9725bf6
AraneusRota <araneusrota.github@paluk.org>
2021-06-17 17:35:43 +0200

    #3: test tape backward propagation by learning factors of a polynomial function by stochastic gradient descent

--- src/main/scala/differentiable/reversemode/tape/Tape.scala
+++ src/main/scala/differentiable/reversemode/tape/Tape.scala
@@ -23,14 +23,25 @@ case class Dual(x: Double, var d: Double):
     tape = ((_: Unit) => r.d += x * y.d) andThen tape
     y
 
+  def **(n: Int): Dual =
+    require(n >= 0)
+    if n == 0 then 1 else this * (this ** (n - 1))
+
 given Conversion[Double, Dual] = Dual(_, 0)
 given Conversion[Int, Dual] = Dual(_, 0)
 
-def grad(f: Dual => Dual)(x: Double): Double =
-  val xDual = Dual(x, 0)
-  f(xDual).d = 1
+def grad(x: Double)(f: Dual => Dual): Double =
+  grad(x :: Nil) { case xDual :: _ =>
+    f(xDual)
+  }
+  .head
+
+def grad(xs: List[Double])(f: List[Dual] => Dual): List[Double] =
+  tape = (_: Unit) => ()
+  val xsDual: List[Dual] = xs map identity
+  f(xsDual).d = 1
   tape(())
-  xDual.d
+  xsDual map { _.d }
 
 @main def main() =
   def f(x: Dual): Dual =
@@ -38,4 +49,4 @@ def grad(f: Dual => Dual)(x: Double): Double =
 
 
   println(f(2))
-  println(grad(f)(2))
+  println(grad(2)(f))
--- src/test/scala/differentiable/reversemode/tape/TapeTest.scala
+++ src/test/scala/differentiable/reversemode/tape/TapeTest.scala
@@ -4,31 +4,93 @@ import org.scalactic.Equality
 import org.scalactic.TolerantNumerics.tolerantDoubleEquality
 import org.scalatest.funsuite.AnyFunSuite
 
+import java.lang.Math.{pow, random}
 import scala.language.implicitConversions
 
 class TapeTest extends AnyFunSuite {
-  implicit val doubleEq: Equality[Double] = tolerantDoubleEquality(0.0001)
 
   test("reverse mode tape differentiation") {
+    implicit val doubleEq: Equality[Double] = tolerantDoubleEquality(0.0001)
     val cases: Seq[(Dual => Dual, Double => Double)] = Seq(
       ((x: Dual) => x * x) ->
         (x => 2 * x),
-      ((x: Dual) => 5.5 * (x * x * x)) ->
+      ((x: Dual) => 5.5 * (x ** 3)) ->
         (x => 16.5 * (x * x)),
       ((x: Dual) => 2 * x + x * x * x) ->
         (x => 2 + 3 * x * x),
       ((x: Dual) => x * 5) ->
-        (x => 5)
+        (x => 5),
+      ((x: Dual) => 0 + 0 * -0.1 + (x ** 8) * (x ** 0) * (x ** 2) + (x ** 4) * x * (x ** 2) * (x ** 1) * (x ** 3)) ->
+        (x => 10 * pow(x, 9) + 11 * pow(x, 10))
     )
 
     cases.foreach { (actual, expectedDerivative) =>
       (-5 to 5).foreach { x =>
         assert(
-          grad(actual)(x) === expectedDerivative(x),
+          grad(x)(actual) === expectedDerivative(x),
           s"\nx = $x\nActual: $actual\nExpected: $expectedDerivative")
 
 
       }
     }
+  }
+
+  test("reverse mode tape learning") {
+    val realWs: List[Double] = List(10, -7, -5, 4)
+    def trainPoly(x: Double)(ws: List[Double]): Double =
+      poly(x)(ws map identity).x
+
+    def poly(x: Double)(ws: List[Dual]): Dual =
+      ws
+        .zipWithIndex
+        .map { (w, i) =>
+          w * (x ** i)
+        }
+        .reduce(_ + _)
+
+    val trainInput = Range.BigDecimal(-5, 5, 1)
+
+    def loss(x: Double)(ws: List[Dual]): Dual =
+      (trainPoly(x)(realWs) + -1 * poly(x)(ws)) ** 2
+
+    def learn(ws: List[Double], rate: Double, stopAcc: Double, maxIterations: Int): List[Double] =
+      val lossAverage =
+        trainInput
+          .map { x =>
+            val wsDual: List[Dual] = ws map identity
+            loss(x.toDouble)(wsDual).x / ws.length / trainInput.length
+          }
+          .sum
+//      println(s"It: $maxIterations Loss: $lossAverage, Current Ws: $ws")
+      if lossAverage < stopAcc || maxIterations <= 0 || !lossAverage.isFinite then
+        ws
+      else
+        val dLossAverage: List[Double] =
+          trainInput
+            .map { x =>
+              grad(ws)(loss(x.toDouble)) map { _ / trainInput.length }
+            }
+            .reduce {
+              _ zip _ map {
+                _ + _
+              }
+            }
+        val newWs =
+          ws zip dLossAverage map { (w, dLoss) =>
+            w - rate / ws.length * dLoss
+          }
+        learn(newWs, rate, stopAcc, maxIterations - 1)
+      end if
+    end learn
+
+    val initialWs = (0 to 3 map { _ => 300 * random - 150 }).toList
+    val acc = 0.00001
+    val learnedWs = learn(initialWs, 0.0007, acc, 100000)
+
+    implicit val doubleEq: Equality[Double] = tolerantDoubleEquality(0.01)
+    assert(realWs zip learnedWs map { _ === _ } reduce { _ && _ }, s"\nreal: $realWs\nlearned: $learnedWs")
+    println(s"Init: $initialWs")
+    println(s"Real: $realWs")
+    println(s"Learned: $learnedWs")
   }
 }
