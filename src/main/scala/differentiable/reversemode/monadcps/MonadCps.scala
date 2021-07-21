6aaa9295ff34605d55a9c676458374ddd41ca438
AraneusRota <araneusrota.github@paluk.org>
2021-07-21 23:24:54 +0200

    #2: adjust monad backpropagation to allow chaining of functions

--- src/main/scala/differentiable/reversemode/monadcps/MonadCps.scala
+++ src/main/scala/differentiable/reversemode/monadcps/MonadCps.scala
@@ -1,5 +1,7 @@
 package differentiable.reversemode.monadcps
 
+import differentiable.reversemode.monadcps.DualMonad.wrap
+
 import java.lang.Math.pow
 import scala.annotation.showAsInfix
 import scala.compiletime.ops.int._
@@ -9,65 +11,69 @@ import scala.language.implicitConversions
 import scala.quoted._
 import concurrent.ExecutionContext.Implicits.global
 
-trait DualMonad:
-  def flatMap(k: Dual => DualMonad): DualMonad
-  def map(k: Dual => Dual): DualMonad
-//  def get: Dual
+var tape: Unit => Unit = _ => {}
+
+class DualMonad(val y: Dual, derivationUpdater: Dual => Unit):
+  def flatMap(k: Dual => DualMonad): DualMonad =
+    tape = ((_: Unit) => derivationUpdater(y)) andThen tape
+    k(y)
+
+  def map(k: Dual => Dual): DualMonad =
+    flatMap(dual => wrap(k(dual)))
+end DualMonad
+
+object DualMonad:
+  def wrap(dual: Dual): DualMonad = DualMonad(dual, identity)
+
 
 case class Dual(x: Double, var d: Double):
-  def +(r: Dual): DualMonad = new DualMonad {
-    override def flatMap(k: Dual => DualMonad): DualMonad =
-      val y = Dual(x + r.x, 0)
-      val res = k(y)
-      d += y.d
-      r.d += y.d
-      res
+  def +(r: Dual): DualMonad = DualMonad(
+    x * r.x,
+    y => {
+      d += y.d // dl(l + r) * y.d = (1 + 0) * y.d
+      r.d += y.d // like above
+    }
+  )
 
-    override def map(k: Dual => Dual): DualMonad =
-      def wrap(dual: Dual): DualMonad = dual + 0
-      flatMap(dual => wrap(k(dual)))
-  }
-
-  def *(r: Dual): DualMonad = new DualMonad {
-    override def flatMap(k: Dual => DualMonad): DualMonad =
-      val y = Dual(x * r.x, 0)
-      val res = k(y)
-      d += r.x * y.d
-      r.d += x * y.d
-      res
-
-    override def map(k: Dual => Dual): DualMonad =
-      def wrap(dual: Dual): DualMonad = dual * 1
-      flatMap(dual => wrap(k(dual)))
-  }
+  def *(r: Dual): DualMonad = DualMonad(
+    x * r.x,
+    y => {
+      d += r.x * y.d // dl(l * r) * y.d = (1 * r) * y.d
+      r.d += x * y.d // like above
+    }
+  )
 end Dual
 
 
 given Conversion[Double, Dual] = Dual(_, 0)
 given Conversion[Int, Dual] = Dual(_, 0)
 
-def grad(f: Dual => (Dual => DualMonad) => DualMonad)(x: Double): Double = {
-  val xDual = Dual(x, 0)
-  f(xDual) { top =>
-    top.d = 1
-    top * 1
-  }
-  xDual.d
-}
+def grad(f: DualMonad => DualMonad)(x: Double): Double =
+  val xDualMonad = wrap(Dual(x, 0))
+  f(xDualMonad).y.d = 1
+  tape(())
+  xDualMonad.y.d
 
 def !(monad: DualMonad): Dual = ???
-// !(2 * x) * x
 
 @main def main() =
-  def f(x: Dual)(k: Dual => DualMonad): DualMonad =
-    for {
-      y1 <- x * 2
-      y2 <- x * x
-      y3 <- y2 * x
-      y4 <- y1 + y3
-      y5 <- k(y4)
-    } yield y4
+  def f(xM: DualMonad): DualMonad =
+      for {
+        x <- xM
+        y1 <- x * 2
+        y2 <- x * x
+        y3 <- y2 * x // y3' = y2' * x + y2 * x'
+        y4 <- y1 + y3
+      } yield y4
   end f
 
-  println(grad(f)(3))
+  def g(xM: DualMonad): DualMonad =
+      for {
+        x <- xM
+        y <- x * x
+      } yield y
+
+
+
+  println(grad(f andThen g)(3))
 end main
