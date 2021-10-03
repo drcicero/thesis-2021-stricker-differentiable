bd0d536843ac9bec09ec342dda429a44adb9b4c6
AraneusRota <araneusrota.github@paluk.org>
2021-10-03 22:16:59 +0200

    latex: forward mode: macros and dual numbers, init match type

---
+++ latex/listings/implementation/DualNumber.scala
@@ -0,0 +1,25 @@
+package differentiable.dualnumber
+
+case class Dual(v: Double, d: Double):
+  def *(that: Dual) = Dual(
+    this.v * that.v,
+    this.v * that.d + this.d * that.v
+  )
+
+  def +(that: Dual) = Dual(
+    this.v + that.v,
+    this.d + that.d
+  )
+
+def const(v: Double) = Dual(v, 0)
+def variable(v: Double) = Dual(v, 1)
+
+@main def mainDualNumber() = {
+def differentiate(f: Dual => Dual)(x: Double): Double =
+  val result: Dual = f(variable(x))
+  result.d // result.v would be the actual result of f(x)
+
+def f(x: Dual): Dual = const(2) * x + x * x * x
+
+differentiate(f)(3)
+}
