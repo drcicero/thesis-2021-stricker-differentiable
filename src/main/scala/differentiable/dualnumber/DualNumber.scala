906558a40cda39d8b4e166d917e4a88f9f6da2e5
AraneusRota <araneusrota.github@paluk.org>
2021-10-03 20:11:07 +0200

    add correct DualNumber forward mode differentiation

--- src/main/scala/differentiable/dualnumber/DualNumber.scala
+++ src/main/scala/differentiable/dualnumber/DualNumber.scala
@@ -1,55 +1,24 @@
 package differentiable.dualnumber
 
-import java.lang.Math.pow
-import scala.annotation.showAsInfix
-import scala.compiletime.ops.int._
-import scala.compiletime.{constValue, erasedValue}
-import scala.quoted._
+case class Dual(v: Double, d: Double):
+  def *(that: Dual) = Dual(
+    this.v * that.v,
+    this.v * that.d + this.d * that.v
+  )
 
-sealed trait Poly:
-  def apply(x: Double): Double
-  def d(x: Double): Double
+  def +(that: Dual) = Dual(
+    this.v + that.v,
+    this.d + that.d
+  )
 
-  def *(r: Poly): Poly = Mul(this, r)
-  def +(r: Poly): Poly = Plus(this, r)
-  def **(n: Int): Poly  = Pow(this, n)
+def const(v: Double) = Dual(v, 0)
+def variable(v: Double) = Dual(v, 1)
 
-case class Mul(l: Poly, r: Poly) extends Poly:
-  override def apply(x: Double): Double = l(x) * r(x)
-  override def d(x: Double): Double =
-    l(x) * r.d(x) + l.d(x) * r(x)
-  override def toString: String = s"$l * $r"
+@main def mainDualNumber() =
+  def differentiate(f: Dual => Dual)(x: Double): Double =
+    val result: Dual = f(variable(x))
+    result.d // result.v would be the actual result of f(x)
 
-case class Plus(l: Poly, r: Poly) extends Poly:
-  override def apply(x: Double): Double = l(x) + r(x)
-  override def d(x: Double): Double = l.d(x) + r.d(x)
-  override def toString: String = s"($l + $r)"
+  def f(x: Dual): Dual = const(2) * x + x * x * x
 
-case class Pow(b: Poly, n: Int) extends Poly:
-  override def apply(x: Double): Double = powOrElseThrow(b(x), n)
-  override def d(x: Double): Double = n match
-    case 0 => 0
-    case 1 => b.d(x)
-    case _ => n * b.d(x) * powOrElseThrow(b(x), n - 1)
-  override def toString: String = s"($b)**$n"
-  private def powOrElseThrow(b: Double, n: Int): Double =
-    val res = pow(b, n)
-    if res.isFinite then res else throw IllegalArgumentException()
-
-object X extends Poly:
-  override def apply(x: Double): Double = x
-  override def d(x: Double): Double = 1
-  override def toString: String = "X"
-
-case class V(v: Double) extends Poly:
-  override def apply(x: Double): Double = v
-  override def d(x: Double): Double = 0
-  override def toString: String = v.toString
-
-given Conversion[Double, V] = V(_)
-given Conversion[Int, V] = V(_)
-
-@main def main() =
-  import scala.language.implicitConversions
-  val f = 6 * 3 * (X**5) + 5
-  println(0 * -0.1)
+  println(differentiate(f)(3))
